6546448459c332ab14178af23d596a73315ea6d4
TJ-Abbing <tjabbing@gmail.com>
2023-01-27 15:21:36 +0100

    Updated; create.

--- frontend/src/components/CreateNote.jsx
+++ frontend/src/components/CreateNote.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export function CreateNote() {
-    const handleSubmit = (event, title, author, inRepertoireSince) => {
+    const handleSubmit = (event, title, author, body) => {
         event.preventDefault();
         const options = {
             method: 'POST',
             headers:  { 'Accept': 'application/json', 'Content-type': 'application/json'},
-            body: JSON.stringify({"title": title, "author": author, "body": inRepertoireSince})
+            body: JSON.stringify({"title": title, "author": author, "body": body})
         }
         const url = 'https://docent.cmi.hro.nl/bootb/demo/notes/'
 
@@ -18,19 +18,19 @@ export function CreateNote() {
 
     const [title, setTitle] = useState();
     const [author, setAuthor] = useState();
-    const [inRepertoireSince, setInRepertoireSince] = useState();
-    console.log(title, author, inRepertoireSince)
+    const [body, setbody] = useState();
+    console.log(title, author, body)
 
     return (
-        <div className="create-song">
-            <Link to={`/`}>Go Back</Link>
-            <form onSubmit={(e) => {handleSubmit(e, title, author, inRepertoireSince)}}>
+        <div>
+            <Link to={`/`}>Return</Link>
+            <form onSubmit={(e) => {handleSubmit(e, title, author, body)}}>
                 <label>title</label>
                 <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>
                 <label>author</label>
                 <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)}></input>
-                <label>inRepertoireSince</label>
-                <input type="text" value={inRepertoireSince} onChange={(e) => setInRepertoireSince(e.target.value)}></input>
+                <label>body</label>
+                <input type="text" value={body} onChange={(e) => setbody(e.target.value)}></input>
                 <input type="submit" value="submit" />
             </form>
         </div>
