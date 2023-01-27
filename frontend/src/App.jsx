81549f0626ddb4f2893ccc9bffba351cd23bdd8a
TJ-Abbing <tjabbing@gmail.com>
2023-01-27 15:07:55 +0100

    Added; create.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Notes } from "./components/Notes";
+import { CreateNote } from "./components/CreateNote";
 
 export function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <Notes /> }></Route>
+                <Route path="/create" element={ <CreateNote /> }></Route>
             </Routes>
         </BrowserRouter>
     );
